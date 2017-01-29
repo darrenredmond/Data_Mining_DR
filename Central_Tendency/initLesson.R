8d25456e5088753b558acd347ec1ca255153e84b
Darren Redmond <c_dredmond@groupon.com>
2017-01-29 10:07:04 +0000

    Update

--- Central_Tendency/initLesson.R
+++ Central_Tendency/initLesson.R
@@ -1,6 +1,3 @@
-if (!require("openintro")) install.packages("openintro")
-library(openintro)
-
 assign("cars", openintro::cars, envir=globalenv())
 assign("mpg.midsize", cars[cars$type=="midsize","mpgCity"], envir=globalenv())
 
